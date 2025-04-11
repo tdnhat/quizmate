8c6c7e5139f5beb2aa84943c3724318546cfc494
tdn283 <nhatflame@gmail.com>
2025-04-11 20:06:56 +0700

    enhance: improve layout and responsiveness in Quiz session components

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -4,6 +4,12 @@ import RecommendedSection from "@/features/discover/components/RecommendedSectio
 const HomePage = () => {
     return (
         <div className="flex flex-col max-w-6xl mx-auto gap-4 p-4">
+            <div className="flex flex-col">
+                <h1 className="text-2xl font-bold">Home</h1>
+                <p className="text-gray-600 mb-6">
+                    Welcome to the home page of QuizMate
+                </p>
+            </div>
             <CategoriesSection />
             <RecommendedSection />
         </div>
